bf6e635e143edf49b6b23972651cc29af9916512
Krzysztof <krzysztof.misztal@gmail.com>
2014-07-15 01:33:00 +0200

    Same plugin base classes

---
+++ src/pl/edu/misztal/imageprocessing/plugins/util/Lookup3DTablePlugin.java
@@ -0,0 +1,34 @@
+package pl.edu.misztal.imageprocessing.plugins.util;
+
+import java.awt.image.BufferedImageOp;
+import java.awt.image.LookupOp;
+import java.awt.image.ShortLookupTable;
+import pl.edu.misztal.imageprocessing.image.Image;
+import pl.edu.misztal.imageprocessing.image.plugins.Plugin;
+import pl.edu.misztal.imageprocessing.image.utils.Attributes;
+import pl.edu.misztal.imageprocessing.image.utils.Mask;
+
+/**
+ * Lookup Table filter with different table for each channel
+ * @author Krzysztof
+ */
+public abstract class Lookup3DTablePlugin extends Plugin {
+
+    protected short[][] LUT;
+
+    protected abstract void createLUT();
+
+    @Override
+    public void process(Image imgIn, Image imgOut, Attributes attrOut, Mask mask) {
+        if (mask != null) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
+        createLUT();
+        
+        final BufferedImageOp bio
+                = new LookupOp(new ShortLookupTable(0, LUT), null);
+        bio.filter(imgIn.getBufferedImage(), imgOut.getBufferedImage());
+    }
+
+}
